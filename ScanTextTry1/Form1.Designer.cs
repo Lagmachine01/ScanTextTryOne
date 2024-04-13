66a1dcabf45c170956c62cb79f4dff3e20b26782
WW <WW@DESKTOP-81UKHMG>
2024-04-13 19:54:05 +0300

    Добавьте файлы проекта.

---
+++ ScanTextTry1/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace ScanTextTry1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenBTN = new System.Windows.Forms.Button();
+            this.ConvBTN = new System.Windows.Forms.Button();
+            this.ConvTextBox = new System.Windows.Forms.RichTextBox();
+            this.ImageBox = new System.Windows.Forms.PictureBox();
+            this.PathBox = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OpenBTN
+            // 
+            this.OpenBTN.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.OpenBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OpenBTN.Location = new System.Drawing.Point(222, 7);
+            this.OpenBTN.Name = "OpenBTN";
+            this.OpenBTN.Size = new System.Drawing.Size(116, 47);
+            this.OpenBTN.TabIndex = 1;
+            this.OpenBTN.Text = "Open image";
+            this.OpenBTN.UseVisualStyleBackColor = false;
+            this.OpenBTN.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ConvBTN
+            // 
+            this.ConvBTN.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ConvBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ConvBTN.Location = new System.Drawing.Point(344, 7);
+            this.ConvBTN.Name = "ConvBTN";
+            this.ConvBTN.Size = new System.Drawing.Size(116, 47);
+            this.ConvBTN.TabIndex = 2;
+            this.ConvBTN.Text = "Convert to text";
+            this.ConvBTN.UseVisualStyleBackColor = false;
+            this.ConvBTN.Click += new System.EventHandler(this.ConvBTN_Click);
+            // 
+            // ConvTextBox
+            // 
+            this.ConvTextBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ConvTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.ConvTextBox.Location = new System.Drawing.Point(3, 414);
+            this.ConvTextBox.Name = "ConvTextBox";
+            this.ConvTextBox.ReadOnly = true;
+            this.ConvTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.ConvTextBox.Size = new System.Drawing.Size(579, 147);
+            this.ConvTextBox.TabIndex = 3;
+            this.ConvTextBox.Text = "";
+            // 
+            // ImageBox
+            // 
+            this.ImageBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ImageBox.Location = new System.Drawing.Point(3, 44);
+            this.ImageBox.Name = "ImageBox";
+            this.ImageBox.Size = new System.Drawing.Size(579, 326);
+            this.ImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ImageBox.TabIndex = 4;
+            this.ImageBox.TabStop = false;
+            // 
+            // PathBox
+            // 
+            this.PathBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.PathBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.PathBox.Location = new System.Drawing.Point(151, 4);
+            this.PathBox.Name = "PathBox";
+            this.PathBox.ReadOnly = true;
+            this.PathBox.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.PathBox.Size = new System.Drawing.Size(431, 34);
+            this.PathBox.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Info;
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(603, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(319, 632);
+            this.panel1.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(8, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(297, 579);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(3, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Found restricted words:\r\n";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.ConvTextBox);
+            this.panel2.Controls.Add(this.ImageBox);
+            this.panel2.Controls.Add(this.PathBox);
+            this.panel2.Location = new System.Drawing.Point(12, 75);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(585, 569);
+            this.panel2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "File path:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(3, 379);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(221, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Converted text:";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.LawnGreen;
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.OpenBTN);
+            this.panel3.Controls.Add(this.ConvBTN);
+            this.panel3.Location = new System.Drawing.Point(12, 11);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(585, 58);
+            this.panel3.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(10, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(189, 32);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Main buttons";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(466, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 47);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Check text";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 656);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button OpenBTN;
+        private System.Windows.Forms.Button ConvBTN;
+        private System.Windows.Forms.RichTextBox ConvTextBox;
+        private System.Windows.Forms.PictureBox ImageBox;
+        private System.Windows.Forms.TextBox PathBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ScanTextTry1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+using AForge;
+
+namespace ScanTextTry1
+{
+    public partial class Form1 : Form
+    {
+        public string openedText = ""; //string that is text, converted from image
+        public int num = 0; //int to show later that all words are not restricted
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+
+                ImageBox.Image = new Bitmap(open.FileName);
+
+                PathBox.Text = open.FileName;
+            }
+        }
+
+        public void ConvBTN_Click(object sender, EventArgs e)
+        {
+            var ocrengine = new TesseractEngine(@".\tessdata", "rus+eng", EngineMode.Default);
+            var img = Pix.LoadFromFile(PathBox.Text);
+            var res = ocrengine.Process(img);
+            ConvTextBox.Text = res.GetText();
+            openedText = res.GetText();
+        }
+
+        public void CheckTheText(string text) //Checks the text for "restricted" words
+        {
+            string[] RestrictedText = { "Sugar", "sugar" };
+            List<string> words = new List<string>(text.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries));
+
+            foreach (string word in words)
+            {
+                string trimmedWord = word.Trim();
+                if (RestrictedText.Any(restrictedWord => string.Equals(restrictedWord, trimmedWord, StringComparison.OrdinalIgnoreCase)))
+                {
+                    listBox1.Items.Add(trimmedWord);
+                }
+                else
+                {
+                    num++;
+                }
+            }
+            if (num == words.Count) listBox1.Items.Add("No restricted words");
+        }
+
+        public void button1_Click_1(object sender, EventArgs e)
+        {
+            if(!(openedText is null)) CheckTheText(openedText);
+        }
+    }
+}
